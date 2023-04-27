cac3b423bd781828023278b3770d773a01d6cbc9
lucassantos22 <lucas.afsantos@hotmail.com>
2023-04-27 18:35:54 -0300

    chore: add Home structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { Container, Header } from './styles';
+
+import avatarDefault from '../../assets/avatar01.jpeg';
 
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Title>Home Page</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton>
+              <UserAvatar source={avatarDefault}></UserAvatar>
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Lucas</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
