4a1347b8fb18d77267c9e6bcb429257cd270fe27
lucassantos22 <lucas.afsantos@hotmail.com>
2023-04-27 18:02:12 -0300

    chore: add fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { Home } from './src/pages/Home';
 import theme from './src/global/styles/theme';
 
 const App: React.FunctionComponent = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Home />
