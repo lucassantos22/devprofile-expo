f6dd8e004e1314a601df48ac26b4eb279040df09
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-14 19:57:01 -0300

    chore: refactor SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useForm, FieldValues } from 'react-hook-form';
 import {
   Container,
   Content,
@@ -11,13 +12,23 @@ import {
   Icon,
   CreateAccountTitle,
 } from './styles';
-import { Input } from '../../components/Form/Input';
+import { InputControl } from '../../components/Form/InputControl';
 import { ScrollView, KeyboardAvoidingView, Platform, View } from 'react-native';
 import { Button } from '../../components/Form/Button';
 import logo from '../../assets/logo.png';
 
+interface IFormInputs {
+  [email: string]: any;
+}
+
 export const SignIn: React.FunctionComponent = () => {
+  const { handleSubmit, control } = useForm<FieldValues>();
   const navigation = useNavigation<any>();
+
+  function handleSignIn({ email, password }: IFormInputs) {
+    console.log(email, password);
+  }
+
   return (
     <KeyboardAvoidingView
       enabled
@@ -34,9 +45,23 @@ export const SignIn: React.FunctionComponent = () => {
             <View>
               <Title>Login</Title>
             </View>
-            <Input placeholder="Email" />
-            <Input placeholder="Password" />
-            <Button title="Login" />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="email"
+              placeholder="Email"
+              keyboardType="email-address"
+            />
+            <InputControl
+              autoCapitalize="none"
+              autoCorrect={false}
+              control={control}
+              name="password"
+              placeholder="Password"
+              secureTextEntry
+            />
+            <Button title="Login" onPress={handleSubmit(handleSignIn)} />
             <ForgotPasswordButton>
               <ForgotPasswordTitle>Forgot my password</ForgotPasswordTitle>
             </ForgotPasswordButton>
