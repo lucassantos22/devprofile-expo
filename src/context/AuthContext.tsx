d674c9834302f04a6532cc2777cab7e19c2133d5
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-18 20:09:06 -0300

    chore: add AsyncStorage and state to AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,12 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/api';
+import { IUser } from '../model/User';
+
+interface IAuthState {
+  token: string;
+  user: IUser;
+}
 
 interface ICredentials {
   email: string;
@@ -7,7 +14,7 @@ interface ICredentials {
 }
 
 interface IAuthContext {
-  name: string;
+  user: IUser;
   signIn(credentials: ICredentials): void;
 }
 
@@ -17,19 +24,41 @@ interface IProps {
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
+const tokenData = '@DevProfile:token';
+const userData = '@DevProfile:user';
+
 export const AuthProvider: React.FunctionComponent<IProps> = ({ children }) => {
+  const [data, setData] = useState<IAuthState>({} as IAuthState);
+
+  useEffect(() => {
+    async function loadAuthData() {
+      const token = await AsyncStorage.getItem(tokenData);
+      const user = await AsyncStorage.getItem(userData);
+
+      if (token && user) {
+        setData({ token, user: JSON.parse(user) });
+      }
+    }
+
+    loadAuthData();
+  }, []);
+
   async function signIn({ email, password }: ICredentials) {
     try {
-      await api.post('sessions', {
+      const res = await api.post('sessions', {
         email,
         password,
       });
+      const { token, user } = res.data;
+      await AsyncStorage.setItem(tokenData, token);
+      await AsyncStorage.setItem(userData, JSON.stringify(user));
+      setData({ token, user });
     } catch (error) {
       throw new Error(error as string);
     }
   }
   return (
-    <AuthContext.Provider value={{ name: 'Lucas', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
