39b02bb50be15742f5fa8678d79195e4363b424f
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-19 14:28:59 -0300

    chore: import useAuth

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,19 +13,25 @@ import {
 } from './styles';
 
 import avatarDefault from '../../assets/avatar01.jpeg';
+import { useAuth } from '../../context/AuthContext';
 
 export const Home: React.FunctionComponent = () => {
+  const { user } = useAuth();
   return (
     <Container>
       <Header>
         <UserWrapper>
           <UserInfo>
             <UserAvatarButton>
-              <UserAvatar source={avatarDefault}></UserAvatar>
+              <UserAvatar
+                source={
+                  user.avatar_url ? { uri: user.avatar_url } : avatarDefault
+                }
+              ></UserAvatar>
             </UserAvatarButton>
             <UserInfoDetail>
               <UserGreeting>Hello, </UserGreeting>
-              <UserName>Lucas</UserName>
+              <UserName>{user.name}</UserName>
             </UserInfoDetail>
           </UserInfo>
           <Icon name="power" />
