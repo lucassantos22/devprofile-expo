98788f69ff768e0ef4ac925f145b9d7a6de72ffc
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-22 20:02:02 -0300

    chore: add routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
+import { ForgotPassword } from '../pages/ForgotPassword';
+import { ResetPassword } from '../pages/ResetPassword';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,6 +12,8 @@ export const AuthRoutes: React.FunctionComponent = () => {
     <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
       <Screen name="SignIn" component={SignIn} />
       <Screen name="SignUp" component={SignUp} />
+      <Screen name="ForgotPassword" component={ForgotPassword} />
+      <Screen name="ResetPassword" component={ResetPassword} />
     </Navigator>
   );
 };
