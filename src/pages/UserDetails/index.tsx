080463db34b46dc6230b52baa0761a976328767c
lucassantos22 <lucas.afsantos@hotmail.com>
2023-05-31 23:56:30 -0300

    chore: get id in user details

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { Container } from './styles';
 import { IUser } from '../../model/User';
@@ -13,7 +13,7 @@ export const UserDetails: React.FunctionComponent = () => {
   const route = useRoute();
   const { userId } = route.params as RouteParams;
 
-  React.useEffect(() => {
+  useEffect(() => {
     const loadUser = async () => {
       const response = await api.get(`/users/${userId}`);
       setUserDetails(response.data);
