2fa0563d1c596b4aae7ab49dd6d5335952dcc1be
lucassantos22 <lucas.afsantos@hotmail.com>
2023-06-13 20:24:50 -0300

    chore: add UserProfileEdit route

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '../pages/Home';
 import { UserDetails } from '../pages/UserDetails';
 import { UserProfile } from '../pages/UserProfile';
+import { UserProfileEdit } from '../pages/UserProfileEdit';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export const AppRoutes: React.FunctionComponent = () => {
       <Screen name="Home" component={Home} />
       <Screen name="UserDetails" component={UserDetails} />
       <Screen name="UserProfile" component={UserProfile} />
+      <Screen name="UserProfileEdit" component={UserProfileEdit} />
     </Navigator>
   );
 };
